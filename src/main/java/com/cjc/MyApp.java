19a8b0d3a53947ccd6dac8a39da32a362e702760
Shubham <24shubhamp@gmail.com>
2021-07-25 15:25:42 +0530

    His-301 Changes

--- src/main/java/com/cjc/MyApp.java
+++ src/main/java/com/cjc/MyApp.java
@@ -3,7 +3,13 @@ package com.cjc;
 public class MyApp {
 	
 	public static void main(String[] args) {
+		private Map<String,Object> cache=new HashMap<>();
 		int i=10;
+	}
+	
+	//HIS-301 Changes
+	public void loadDataToCache(){
+		//logic
 	}
 
 }
